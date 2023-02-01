57e76d4069afebdded37be97a003e3eac97f8df5
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-02-01 18:44:08 +0000

    display

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -63,7 +63,7 @@ HTMLActuator.prototype.addTile = function (tile) {
 
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  inner.textContent = tile.value >= 1e5 ? (tile.value ** 2).toPrecision(5) : (tile.value ** 2).toString();
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
